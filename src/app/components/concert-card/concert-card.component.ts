d1ea0e319213980f17a6b0b3485babc9f03e2bf0
Marci0817 <mihmar17@gmail.com>
2025-05-18 16:12:59 +0200

    Add new pipe

--- src/app/components/concert-card/concert-card.component.ts
+++ src/app/components/concert-card/concert-card.component.ts
@@ -6,6 +6,7 @@ import { MatCardHeader } from '@angular/material/card';
 import { MatCardTitle } from '@angular/material/card';
 import { MatCardSubtitle } from '@angular/material/card';
 import { MatCardContent } from '@angular/material/card';
+import { CapitalizePipe } from '../../pipes//capitalize.pipe';
 import { PriceFormatterPipe } from '../../pipes/price-formatter.pipe';
 @Component({
   selector: 'app-concert-card',
@@ -19,6 +20,7 @@ import { PriceFormatterPipe } from '../../pipes/price-formatter.pipe';
     MatCardSubtitle,
     MatCardContent,
     PriceFormatterPipe,
+    CapitalizePipe,
   ],
 })
 export class ConcertCardComponent {
@@ -29,5 +31,16 @@ export class ConcertCardComponent {
   buyTicket() {
     console.log(`Jegy vásárlása: ${this.concert.name}`);
     this.ticketPurchased.emit(this.concert);
+  }
+
+  ngOnChanges(changes: any) {
+    for (const inputName in changes) {
+      const inputValues = changes[inputName];
+      console.log(inputValues.currentValue);
+    }
+  }
+
+  afterNextRender(changes: any) {
+    fetch('https://randomuser.me/api/').then(console.log);
   }
 }
